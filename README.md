1cce3b03a2e2ec604dab80f1087257bebc680458
AlexV <asmodehn@gmail.com>
2019-04-28 16:34:09 +0100

    first basic content with esp32 things

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # esp32-playground
 Various ESP32 things
+
+# ESP Doc links
+https://docs.espressif.com/projects/esp-idf/en/latest/get-started/linux-setup-scratch.html
---
+++ ubuntu-dependencies.sh
@@ -0,0 +1,6 @@
+# ESP
+apt-get install gawk gperf grep gettext python python-dev automake bison flex texinfo help2man libtool libtool-bin
+# ESP-IDF
+apt-get install gcc git wget make libncurses-dev flex bison gperf python python-pip python-setuptools python-serial python-cryptography python-future python-pyparsing python-pyelftools
+# Playground
+pip install --user pipenv
